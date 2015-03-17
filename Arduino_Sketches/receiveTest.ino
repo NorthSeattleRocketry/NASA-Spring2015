a4284a969732a0cd81656b81608071af8b19afa8
Cody Brewer <cody.brewer.1@gmail.com>
2015-03-16 18:43:09 -0700

    Add Redboard test sketches for transmitting over xBee radio.

---
+++ Arduino_Sketches/receiveTest.ino
@@ -0,0 +1,12 @@
+// Test sketch for receiving data over xBee radio.
+// Any data received over serial in will be converted to 
+// a string and shown on the serial monitor.
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  while(Serial.available() > 0){
+    Serial.println(Serial.readString());
+  }
+}
